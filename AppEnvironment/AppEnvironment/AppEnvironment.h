cc899442efa571d3bb7372bbfc779397570b2e26
ZXIOT <info@zx-iot.cn>
2019-09-10 11:24:30 +0800

    初始化AppEnvironmentType 协议

---
+++ AppEnvironment/AppEnvironment/AppEnvironment.h
@@ -0,0 +1,19 @@
+//
+//  AppEnvironment.h
+//  AppEnvironment
+//
+//  Created by W&Z on 2019/9/10.
+//  Copyright © 2019 W&Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppEnvironment.
+FOUNDATION_EXPORT double AppEnvironmentVersionNumber;
+
+//! Project version string for AppEnvironment.
+FOUNDATION_EXPORT const unsigned char AppEnvironmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppEnvironment/PublicHeader.h>
+
+
